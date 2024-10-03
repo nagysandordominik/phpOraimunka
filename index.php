48a66f0f0722a0790383726d8836f524399e9b54
Sándor Nagy <thesatyka@gmail.com>
2024-10-03 11:16:49 +0200

    end of lesson

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php
+    // TÖMBÖK
     echo '<h1>Tömbök</h1>';
     $nevek = array('alma', 'körte', 'szilva');
     echo "<p>$nevek[1]</p>";
@@ -29,8 +30,78 @@
     $gazda = array('Buksi' => 'Tercsi', 'fifi' => 'Kata', 'michelangelo' => 'Józsi');
     echo "<p> Buksi gazdája {$gazda['Buksi']}</p>";
 
+    echo "<p><ul><li>". implode("</li><li>", $napok) . "</li></ul></p>";
+
+    // STRINGEK
     echo '<h1>Stringek</h1>';
+
+    $nev = "Mekk Elek";
+    $nev2 = "Makk Marci";
+
+    echo "<p> $nev </p>\n";
+
+    echo "<p>Van különbség a \" és az  \" között</p>\n";
+
+    echo "<p>A " .$nev. " és ".$nev2. " barátok</p>\n";
+
+    $minta = "/hello/i";
+    $szoveg = "Hello world, hello everyone!";
     
+    echo "<p> A minta a szövegben:  ". (preg_match_all($minta, $szoveg) ? "benne van" : "nincs benne"). "</p>";
+
+    // Készítsenek egy osztályt, játék lebonyolításhoz, osztályban legyen egy tömb, amiben a játékosok nevei vannak, egy metódus, 
+    // amivel játékost hozzá lehet adni,
+    // illetve ami kiírja a játékosokat, 
+    // és olyan ami kialakít egy véletlen sorrendet, 
+    // és egy olyan ami elforgatja a tömböt és kiírja a játékosokat.
+    // és egy olyan ami visszaadja a játékosok számát.
+
+    class Game{
+        private $players = [];
+
+        // játékos hozzáadása
+        public function addPlayer($playerName){
+            $this->players[] = $playerName;
+        }
+        public function getPlayers(){
+            return count($this->players);
+        }
+        public function printPlayers(){
+            echo "<p><ul><li>". implode("</li><li>", $this->players) . "</li></ul></p>";
+        }
+        public function rotatePlayers() {
+            for ($i = 0; $i < count($this->players); $i++) {
+                array_push($this->players, array_shift($this->players));
+                $this->printPlayers();   
+        }
+        }
+        public function randomize(){
+            shuffle($this->players);
+            $this->printPlayers();
+        }
+        
+    }
+
+    $game = new Game();
+        $game->addPlayer("Józsi");
+        $game->addPlayer("Karcsi");
+        $game->addPlayer("Béla");
+        $game->addPlayer("Kata");
+        $game->addPlayer("Erzsi");
+        $game->addPlayer("Teri");
+
+
+        echo "Játékosok:<br>";
+        $game->printPlayers();
+
+        echo "Megcserélt sorrend:<br>";
+        $game->rotatePlayers();
+
+        echo "Randomizált sorrend:<br>";
+        $game->randomize();
+
+        
+        echo "Játékosok száma: " . $game->getPlayers();
     
     ?>
 </body>
